acd234c031dcc12a244523fc685a2aa741fbaa90
Adam Berger <adam@ratchetdesigns.com>
2020-09-08 15:49:31 +0000

    test(plugin/reconciler): add actual assertions to basic reconciler test

--- packages/plugin/tests/reconciler.test.tsx
+++ packages/plugin/tests/reconciler.test.tsx
@@ -6,15 +6,63 @@ describe('reconcile', () => {
   it('should work', () => {
     return new Promise((resolve, reject) => {
       const onCommit = (_: any, updates: Array<ReconciliationUpdate>) => {
-        console.log(JSON.stringify(updates));
+        // TODO: write an expect.anyCapture that can capture named values
+        //       to assert that they are the same when used multiple times.
+        //       unification for the win.
+        expect(updates).toContainEqual({
+          nodeId: expect.any(Number),
+          type: 'text',
+          textUpdate: { text: "This is me!" }
+        });
+        expect(updates).toContainEqual({
+          nodeId: expect.any(Number),
+          type: 'p',
+          propUpdates: []
+        });
+        expect(updates).toContainEqual({
+          nodeId: expect.any(Number),
+          type: 'p',
+          childUpdates: [{
+            op: 'set',
+            childIdx: 0,
+            childId: expect.any(Number)
+          }]
+        });
+        expect(updates).toContainEqual({
+          nodeId: expect.any(Number),
+          type: 'div',
+          propUpdates: [{
+            op: 'set',
+            prop: 'className',
+            value: 'helloWorld'
+          }]
+        });
+        expect(updates).toContainEqual({
+          nodeId: expect.any(Number),
+          type: 'div',
+          childUpdates: [{
+            op: 'set',
+            childIdx: 0,
+            childId: expect.any(Number)
+          }]
+        });
+        expect(updates).toContainEqual({
+          nodeId: 0,
+          type: 'root',
+          childUpdates: [{
+            op: 'set',
+            childIdx: 0,
+            childId: expect.any(Number)
+          }]
+        });
         resolve();
       };
       const root = createRootNode(onCommit);
       render(
         <div className="helloWorld">
-          <div>
+          <p>
             This is me!
-          </div>
+          </p>
         </div>,
         root
       );
