76f364c6156a947bcae730cd9e7ff4de5136a777
Adam Berger <adam@ratchetdesigns.com>
2020-09-21 19:53:52 +0000

    test(test-utils): create a test-utils package for shared test utilities

---
+++ packages/test-utils/README.md
@@ -0,0 +1,11 @@
+# `@pluginsdotdev/test-utils`
+
+> TODO: description
+
+## Usage
+
+```
+const testUtils = require('@pluginsdotdev/test-utils');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/test-utils/src/browser-test.ts
@@ -0,0 +1,54 @@
+import puppeteer from "puppeteer";
+import { startServer as startTestServer } from "./test-server";
+
+import type { Browser, Page } from "puppeteer";
+
+const browserTest = (ports: Array<number>, initialPage: string) => {
+  let _browser: Browser;
+  let _page: Page;
+  let shutdown: () => Promise<undefined>;
+
+  return {
+    browser() {
+      return _browser;
+    },
+
+    page() {
+      return _page;
+    },
+
+    async beforeAll() {
+      const shutdowns = await Promise.all(ports.map(startTestServer));
+
+      shutdown = async () => {
+        await Promise.all(shutdowns.map((shutdown) => shutdown()));
+        return void 0;
+      };
+    },
+
+    async afterAll() {
+      return shutdown();
+    },
+
+    async beforeEach() {
+      _browser = await puppeteer.launch({
+        args: [
+          "--disable-dev-shm-usage", // running in docker with small /dev/shm
+          "--no-sandbox",
+        ],
+      });
+      _page = await _browser.newPage();
+      _page.on("console", (c) => console.log("Log from puppeteer", c.text()));
+      _page.on("error", (e) => console.log("Error from puppeteer", e));
+      await _page.goto(initialPage);
+      // wait until our js has been loaded
+      await _page.waitForFunction(() => !!(<any>window).index);
+    },
+
+    async afterEach() {
+      return _browser.close();
+    },
+  };
+};
+
+export { browserTest };
