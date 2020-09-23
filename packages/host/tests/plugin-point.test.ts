10dfeb3ae59a7db121af5059850043339670a741
Adam Berger <adam@ratchetdesigns.com>
2020-09-23 08:17:16 +0000

    test(host/plugin-point): test passing a function as a prop to the plugin

--- packages/host/tests/plugin-point.test.ts
+++ packages/host/tests/plugin-point.test.ts
@@ -86,4 +86,36 @@ describe("plugin-point", () => {
       "world!"
     );
   });
+
+  it("plugin->host events should work", async () => {
+    const page = t.page();
+    await page.evaluate(() => {
+      const PP = () => {
+        const [count, setCount] = window.React.useState(0);
+
+        return window.React.createElement((<any>window).index.PluginPoint, {
+          hostId: "my-host",
+          pluginPoint: "my-plugin-point",
+          jwt: "fake-jwt",
+          pluginUrl: "http://localhost:8081/tests/plugin.html",
+          props: {
+            className: "hello",
+            title: "world",
+            count: count,
+            onClick: () => setCount(count + 1),
+          },
+        });
+      };
+      window.ReactDOM.render(
+        window.React.createElement(PP, {}),
+        document.getElementById("root")
+      );
+    });
+    await page.waitForSelector("div.hello");
+    expect(await page.$("div.hello")).toBeTruthy();
+    expect(await page.$eval("div.hello > p", (p) => p.innerHTML)).toEqual(
+      "world"
+    );
+    expect(await page.$("button")).toBeTruthy();
+  });
 });
