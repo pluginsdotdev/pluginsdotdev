fe4c8da68d510c550a01d501294d37e32b4d96c7
Adam Berger <adam@ratchetdesigns.com>
2020-09-24 13:49:40 +0000

    feat(host/plugin-point): add a proxy handler for SyntheticEvents

--- packages/host/src/plugin-point.ts
+++ packages/host/src/plugin-point.ts
@@ -1,8 +1,12 @@
 import React from "react";
-import { initializeHostBridge } from "@pluginsdotdev/bridge";
+import {
+  initializeHostBridge,
+  registerFromBridgeProxyHandler,
+  registerToBridgeProxyHandler,
+} from "@pluginsdotdev/bridge";
 import { applyUpdates, emptyRootNode } from "./update-utils";
 
-import type { ComponentType } from "react";
+import type { ComponentType, SyntheticEvent } from "react";
 import type {
   HostId,
   RenderRootId,
@@ -11,6 +15,99 @@ import type {
   NodeId,
 } from "@pluginsdotdev/bridge";
 import type { Node, RootNode } from "./update-utils";
+import type {
+  ProxyIdFactory,
+  LocalBridgeState,
+  HostValue,
+} from "@pluginsdotdev/bridge";
+
+/**
+ * From https://reactjs.org/docs/events.html
+ **/
+const syntheticEventTypes = [
+  { prop: "bubbles", type: "boolean" },
+  { prop: "cancelable", type: "boolean" },
+  { prop: "defaultPrevented", type: "boolean" },
+  { prop: "eventPhase", type: "number" },
+  { prop: "isTrusted", type: "boolean" },
+  { prop: "preventDefault", type: "function" },
+  { prop: "isDefaultPrevented", type: "function" },
+  { prop: "stopPropagation", type: "function" },
+  { prop: "isPropagationStopped", type: "function" },
+  { prop: "persist", type: "function" },
+  { prop: "timeStamp", type: "number" },
+  { prop: "type", type: "string" },
+];
+
+/**
+ * From https://reactjs.org/docs/events.html
+ **/
+const syntheticEventSupers = [
+  { prop: "currentTarget", spr: EventTarget },
+  { prop: "nativeEvent", spr: Event },
+  { prop: "target", spr: EventTarget },
+];
+
+const isSyntheticEvent = (e: any): e is SyntheticEvent => {
+  if (Object(e) !== e) {
+    return false;
+  }
+
+  const rightTypes = syntheticEventTypes.every(
+    ({ prop, type }) => typeof e[prop] === type
+  );
+  const rightSupers = syntheticEventSupers.every(
+    ({ prop, spr }) => e[prop] instanceof spr
+  );
+  return rightTypes && rightSupers;
+};
+
+// TODO: recursive structures?
+const toSimpleObj = (orig: { [key: string]: any }) => {
+  return Object.keys(orig).reduce((o, key) => {
+    const val = orig[key];
+
+    if (key.startsWith("_") || key === "nativeEvent" || key === "view") {
+      // no point in sending these
+      return o;
+    }
+
+    if (typeof val === "function") {
+      // we don't pass any synthetic event functions at this point
+      return o;
+    }
+
+    if (val instanceof EventTarget) {
+      o[key] = {
+        checked: (val as any).checked,
+        value: (val as any).value,
+        selectedIndex: (val as any).selectedIndex,
+      };
+      return o;
+    }
+
+    if (Object(val) === val) {
+      o[key] = toSimpleObj(val);
+      return o;
+    }
+
+    o[key] = val;
+    return o;
+  }, {} as { [key: string]: any });
+};
+
+registerToBridgeProxyHandler(
+  "plugins.dev/SyntheticEvent",
+  (_: ProxyIdFactory, localState: LocalBridgeState, hostValue: HostValue) => {
+    if (!isSyntheticEvent(hostValue)) {
+      return null;
+    }
+
+    return {
+      replacementValue: toSimpleObj(hostValue),
+    };
+  }
+);
 
 type NodeComponentProps = {
   node: Node | undefined;
