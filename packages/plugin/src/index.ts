1c0421caa4ce40059924e11ee2a5883df470e7ab
Adam Berger <adam@ratchetdesigns.com>
2021-01-17 18:00:19 -0500

    feat(plugin/mutation-observing-plugin): start on mutation-observer as a better primitive on which to build react-plugin

--- packages/plugin/src/index.ts
+++ packages/plugin/src/index.ts
@@ -1,3 +1,4 @@
 export * from "./reconciler";
 export * from "./react-plugin";
 export * from "./safe-id";
+export * from "./mutation-observing-plugin"
---
+++ packages/plugin/src/mutation-observing-plugin.ts
@@ -0,0 +1,229 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import {
+  initializePluginBridge,
+  RenderRootId,
+  ReconciliationUpdate,
+  ReconciliationPropUpdate
+} from "@pluginsdotdev/bridge";
+
+import type { ComponentType, ElementType } from "react";
+import type { PluginBridge, Props } from "@pluginsdotdev/bridge";
+
+type ExposedComponents = Record<
+  keyof JSX.IntrinsicElements,
+  keyof JSX.IntrinsicElements
+>;
+
+interface PluginConfig {
+  pluginId: string;
+  hostId: string;
+  userId: string;
+  exposedComponents: ExposedComponents;
+}
+
+type PluginFactory = (pluginConfig: PluginConfig) => ComponentType<Props>;
+
+interface BrowserData {
+  pluginId: string;
+  hostId: string;
+  userId: string;
+  hostOrigin: string;
+  exposedComponentsList: Array<keyof ExposedComponents>;
+}
+
+const browserData = async (): Promise<BrowserData> => {
+  return new Promise((resolve, reject) => {
+    document.addEventListener("DOMContentLoaded", () => {
+      resolve({
+        pluginId: document.body.getAttribute("data-plugin-id")!,
+        hostId: document.body.getAttribute("data-host-id")!,
+        userId: document.body.getAttribute("data-user-id")!,
+        hostOrigin: document.body.getAttribute("data-host-origin")!,
+        exposedComponentsList: JSON.parse(
+          document.body.getAttribute("data-exposed-components")!
+        ) as Array<keyof ExposedComponents>,
+      });
+    });
+  });
+};
+
+const makeExposedComponents = (
+  exposedComponentsList: Array<keyof ExposedComponents>
+): ExposedComponents => {
+  return exposedComponentsList.reduce((exposedComponents, component) => {
+    exposedComponents[
+      component
+    ] = `host:${component}` as keyof JSX.IntrinsicElements;
+    return exposedComponents;
+  }, {} as ExposedComponents);
+};
+
+class NodeIdContainer {
+  private nextId = 0;
+  private nodesById = new WeakMap<Node, number>();
+
+  constructor() { }
+
+  addNode(node: Node): number {
+    const id = this.nextId++;
+    this.nodesById.set(node, id);
+    return id;
+  }
+
+  getOrAddNode(node: Node): number {
+    const id = this.getId(node);
+    return typeof id === 'undefined'
+      ? this.addNode(node)
+      : id;
+  }
+
+  getId(node: Node): number | undefined {
+    return this.nodesById.get(node);
+  }
+
+  isRoot(node: Node): boolean {
+    return this.getId(node) === 0;
+  }
+}
+
+const calculateChildIdx = (node: Node): number => {
+  let childIdx = 0;
+  let currentNode = node;
+  while ( currentNode.previousSibling !== null ) {
+    currentNode = currentNode.previousSibling;
+    ++childIdx;
+  }
+  return childIdx;
+};
+
+type Updates = Record<string, ReconciliationUpdate>;
+
+const mergeUpdates = (a: Updates, b: Updates): Updates => {
+  const allKeys = Array.from(
+    new Set(Object.keys(a).concat(Object.keys(b)))
+  );
+  return allKeys.reduce((result, k) => {
+    const first = result[k];
+    const second = b[k];
+
+    if ( !first ) {
+      result[k] = second;
+      return result;
+    }
+
+    if ( !second ) {
+      return result;
+    }
+
+    first.childUpdates = (first.childUpdates || []).concat(second.childUpdates || []);
+    first.propUpdates = (first.propUpdates || []).concat(second.propUpdates || []);
+    first.textUpdate = first.textUpdate || second.textUpdate;
+    return result;
+  }, a);
+};
+
+const getTreeUpdates = (nodeIdContainer: NodeIdContainer, target: Node, child: Node): Record<string, ReconciliationUpdate> => {
+  const childId = nodeIdContainer.getOrAddNode(child);
+  const targetId = nodeIdContainer.getId(target)!;
+  const targetUpdate: ReconciliationUpdate = {
+    nodeId: targetId,
+    type: nodeIdContainer.isRoot(target)
+        ? "root"
+        : target.nodeName.toLowerCase(),
+    childUpdates: [
+      {
+        op: "set",
+        childIdx: calculateChildIdx(child),
+        childId
+      }
+    ]
+  };
+  // TODO: handle other node types https://developer.mozilla.org/en-US/docs/Web/API/Node/nodeType
+  const attrs: Array<Attr> = child.nodeType === Node.ELEMENT_NODE
+                           ? Array.from((child as Element).attributes)
+                           : [];
+  const propUpdates: Array<ReconciliationPropUpdate> = attrs.map(
+    attr => ({
+      op: "set",
+      prop: attr.name,
+      value: attr.value
+    })
+  );
+  const childUpdate: ReconciliationUpdate = child.nodeType === Node.TEXT_NODE
+    ? {
+      nodeId: childId,
+      type: "text",
+      textUpdate: {
+        text: child.textContent || ''
+      }
+    } : {
+      nodeId: childId,
+      type: child.nodeName.toLowerCase(),
+      propUpdates
+    };
+  const updates: Updates = {
+    [targetId]: targetUpdate,
+    [childId]: childUpdate
+  };
+  const children: Array<Node> = Array.from(child.childNodes);
+  return children.reduce(
+    (updates: Updates, grandchild: Node): Updates => {
+      const treeUpdates = getTreeUpdates(nodeIdContainer, child, grandchild);
+      return mergeUpdates(updates, treeUpdates);
+    },
+    updates
+  );
+};
+
+const registerPlugin = async (pluginFactory: PluginFactory) => {
+  const {
+    hostOrigin,
+    exposedComponentsList,
+    ...pluginConfig
+  } = await browserData();
+  const plugin = pluginFactory({
+    ...pluginConfig,
+    exposedComponents: makeExposedComponents(exposedComponentsList),
+  });
+  const pluginBridge: PluginBridge = await initializePluginBridge(hostOrigin, onRender);
+
+  function onRender(rootId: RenderRootId, props: Props) {
+    const root = document.createElement('div');
+    document.body.appendChild(root);
+
+    const nodeIdContainer = new NodeIdContainer();
+    nodeIdContainer.addNode(root);
+    const obs = new MutationObserver((mutationList, observer) => {
+      const updates = mutationList.reduce(
+        (updatesById, {type, target, addedNodes}) => {
+          const added = Array.from(addedNodes);
+          return added.reduce(
+            (updates, node) => mergeUpdates(updates, getTreeUpdates(nodeIdContainer, target, node)),
+            updatesById
+          );
+        },
+        {} as Updates
+      );
+      pluginBridge.reconcile(rootId, Object.values(updates));
+      console.log(mutationList);
+    });
+
+    obs.observe(
+      root,
+      {
+        attributes: true,
+        childList: true,
+        subtree: true,
+        characterData: true
+      }
+    );
+
+    ReactDOM.render(
+      React.createElement(plugin, props),
+      root
+    );
+  }
+};
+
+export { registerPlugin };
